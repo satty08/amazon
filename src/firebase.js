f86be86dc43298ce27076c10b7ebae9cd0b0ac8c
Satyam <satyambhardawaj@gmail.com>
2020-09-11 12:24:32 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCgkl-FtN0lZuybW_JpZ1DeOcU4NBmBqtQ",
+    authDomain: "fir-5d560.firebaseapp.com",
+    databaseURL: "https://fir-5d560.firebaseio.com",
+    projectId: "fir-5d560",
+    storageBucket: "fir-5d560.appspot.com",
+    messagingSenderId: "182217760689",
+    appId: "1:182217760689:web:a4cbb5f2dbe1c29157e790",
+    measurementId: "G-271WTTXXZ0"
+  };
