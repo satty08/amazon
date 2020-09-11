e2c4519c62d31f3619496915aa6d21b95f258387
Satyam <satyambhardawaj@gmail.com>
2020-09-11 14:49:00 +0530

    header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to Amazon</h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
+      {/*  */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search'
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                {/* Logo */}
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionOne">Hello Guest</span>
+                    <span className="header__optionTwo">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionOne">Returns</span>
+                    <span className="header__optionTwo">& Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionOne">Your</span>  
+                    <span className="header__optionTwo">Prime</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
