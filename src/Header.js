5e1d1996bc7caa7d951eba7dd87ed0fcf40eb234
Satyam <satyambhardawaj@gmail.com>
2020-09-14 22:52:25 +0530

    checkout page

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search'
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="header">
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            </Link>
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
                 {/* Logo */}
@@ -25,10 +32,14 @@ function Header() {
                     <span className="header__optionOne">Your</span>  
                     <span className="header__optionTwo">Prime</span>
                 </div>
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionTwo header__basketCount">0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                            <span className="header__optionTwo header__basketCount">
+                                {basket?.length}
+                            </span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
