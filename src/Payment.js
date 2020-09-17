13bc8e2c505cc2c0c2b1c228133e4e1ad68c4d5b
Satyam <satyambhardawaj@gmail.com>
2020-09-17 20:19:34 +0530

    updates

--- src/Payment.js
+++ src/Payment.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
 import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom'
 
 function Payment() {
 
@@ -10,6 +11,9 @@ function Payment() {
     return (
         <div className="payment">
             <div className="payment__container">
+
+                <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
+
                 <div className="payment__section">
                     <div className="payment__title">
                         <h3>Delivery Address</h3>
@@ -36,7 +40,14 @@ function Payment() {
                         ))}
                     </div>
                 </div>
-                <div className="payment__section"></div>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+                        {/* Stripe Payment */}
+                    </div>
+                </div>
             </div>
         </div>
     )
